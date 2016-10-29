0ea8bdc676c13105a8e14e2baf4d76e0ad05c1b1
paezjm86 <paezjm86@gmail.com>
2016-10-29 10:39:21 -0500

    Agregando la pagina de /signin

--- src/signin/template.js
+++ src/signin/template.js
@@ -1,32 +1,29 @@
 var yo = require('yo-yo');
 var landing = require('../landing');
 
-var signupForm =  yo`<div class="col s12 m7">
+var signinForm = yo`<div class="col s12 m7">
    <div class="row">
       <div class="signup-box">
          <h1 class="platzigram">Platzigram</h1>
          <form class="signup-form">
-            <h2>Registrate para ver fotos de tus amigos en Platzi</h2>
             <div class="section">
                <a class="btn btn-fb hide-on-small-only">Iniciar sesion con Facebook</a>
                <a class="btn btn-fb hide-on-med-and-up">Iniciar secion</a>
             </div>
             <div class="divider"></div>
             <div class="section">
-               <input type="email" name="email" placeholder="Correo electronico"/>
-               <input type="text" name="name" placeholder="Nombre completo"/>
                <input type="text" name="username" placeholder="Nombre de usuario"/>
                <input type="password" name="password" placeholder="Contraseña"/>
-               <button class="btn waves-effect wabes-light btn-signup" type="submit">Registrate</button>
+               <button class="btn waves-effect wabes-light btn-signup" type="submit">Inicia sesion</button>
             </div>
          </form>
       </div>
    </div>
    <div class="row">
       <div class="login-box">
-         ¿Tienes una cuenta? <a href="signin">Entrar</a>
+         ¿No tienes una cuenta? <a href="signin">Registrate</a>
       </div>
    </div>   
 </div>`;
 
-module.exports = landing(signupForm);
+module.exports = landing (signinForm);
