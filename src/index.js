6f6b860e462aadf8f127b004c75c3c420ee2a6f2
paezjm86 <paezjm86@gmail.com>
2016-11-24 12:36:18 -0600

    Cambiando el idioma y almacenandolo en localStorage

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ var page = require('page');
 require('./homepage');
 require('./signup');
 require('./signin');
-
+require('./footer');
 
 page();
--- src/translate/es.js
+++ src/translate/es.js
@@ -1,4 +1,19 @@
 module.exports = {
-   likes: '{ likes, number} me gusta'  
+   'likes': '{ likes, number} me gusta',
+   'logout': 'Salir',
+   'english': 'Ingles',
+   'spanish': 'Español',
+   'signup.subheading': 'Registrate para ver fotos de tus amigos en Platzi',
+   'signup.facebook': 'Iniciar sessión con Facebook',
+   'signup.text': 'Iniciar sessión',
+   'email': 'Correo electrónico',
+   'username': 'Nombre de usuario',
+   'fullname': 'Nombre completo',
+   'password': 'Contraseña',
+   'signup.call-to-action': 'Registrate',
+   'signup.have-account': '¿Tienes una cuenta?',
+   'signin': 'Entrar',
+   'signin.not-have-account': '¿No tienes una cuenta',
+   'language': 'Idioma'
 }
 
