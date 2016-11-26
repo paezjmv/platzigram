0dedc11991d4da945bff138567cb68e47bf60e86
paezjm86 <paezjm86@gmail.com>
2016-11-26 13:40:37 -0600

    Obteniendo una respuesta del servidor con Superagent

--- server.js
+++ server.js
@@ -20,6 +20,35 @@ app.get('/signin', function (req, res){
    res.render('index', {title: 'Platzigram - Signin'});
 })
 
+app.get('/api/pictures', function (req, res) {
+   var pictures = [
+      {
+         user: {
+            username: 'paezjm86',
+            avatar: 'https://pbs.twimg.com/profile_images/583707183180685313/z-K-gc3h.jpg'
+         },
+         url: 'office.jpg',
+         likes: 0,
+         liked: false,
+         createdAt: new Date().getTime()
+      },
+      {
+         user: {
+            username: 'paezjm86',
+            avatar: 'https://pbs.twimg.com/profile_images/583707183180685313/z-K-gc3h.jpg'
+         },
+         url: 'office.jpg',
+         likes: 1,
+         liked: true,
+         createdAt: new Date().setDate(new Date().getDate() - 10)
+      }
+   ];
+
+   setTimeout(function(){
+      res.send(pictures);
+   }, 2000)
+})
+
 app.listen(3000, function (err) {
    if (err) return console.log('Hubo un error'), process.exit(1);
 
--- src/homepage/index.js
+++ src/homepage/index.js
@@ -2,34 +2,24 @@ var page = require('page');
 var empty = require('empty-element');
 var template = require('./template');
 var title = require('title');
+var request = require('superagent');
+var header = require('../header');
 
-page('/', function (ctx, next){
+page('/', header, loadPictures, function (ctx, next){
    title('Platzigram');
    var main = document.getElementById('main-container');
 
-   var pictures = [
-      {
-         user: {
-            username: 'paezjm86',
-            avatar: 'https://pbs.twimg.com/profile_images/583707183180685313/z-K-gc3h.jpg'
-         },
-         url: 'office.jpg',
-         likes: 0,
-         liked: false,
-         createdAt: new Date()
-      },
-      {
-         user: {
-            username: 'paezjm86',
-            avatar: 'https://pbs.twimg.com/profile_images/583707183180685313/z-K-gc3h.jpg'
-         },
-         url: 'office.jpg',
-         likes: 1,
-         liked: true,
-         createdAt: new Date().setDate(new Date().getDate() - 10)
-      }
-   ];
+   empty(main).appendChild(template(ctx.pictures));   
+})
 
-   empty(main).appendChild(template(pictures));   
-}) 
+function loadPictures(ctx, next) {
+   request  
+      .get('/api/pictures')
+      .end(function (err, res){
+         if (err) return console.log(err);
+
+         ctx.pictures = res.body;
+         next();
+      })
+}
 
