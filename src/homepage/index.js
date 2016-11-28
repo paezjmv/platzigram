c6bebf5095d9e53e2492eb70c8e972425cbb0674
paezjm86 <paezjm86@gmail.com>
2016-11-28 14:11:41 -0600

    Obteniendo respuesta del servidor con Fetch API

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -4,8 +4,9 @@ var template = require('./template');
 var title = require('title');
 var request = require('superagent');
 var header = require('../header');
+var axios = require('axios');
 
-page('/', header, loadPictures, function (ctx, next){
+page('/', header, loadPicturesFetch, function (ctx, next){
    title('Platzigram');
    var main = document.getElementById('main-container');
 
@@ -13,13 +14,39 @@ page('/', header, loadPictures, function (ctx, next){
 })
 
 function loadPictures(ctx, next) {
-   request  
+   request
       .get('/api/pictures')
-      .end(function (err, res){
+      .end(function (err, res) {
          if (err) return console.log(err);
 
          ctx.pictures = res.body;
          next();
       })
+}
+
+function loadPicturesAxios(ctx, next) {
+   axios  
+      .get('/api/pictures')
+      .then(function (res) {
+         ctx.pictures = res.data;
+         next();
+      })
+      .catch(function(err) {
+         console.log(err);
+      })
+}
+
+function loadPicturesFetch (ctx, next) {
+   fetch('/api/pictures')
+   .then(function (res) {
+      return res.json();
+   })
+   .then(function (pictures) {
+      ctx.pictures = pictures;
+         next();
+      })
+      .catch(function(err) {
+         console.log(err);
+   })
 }
 
