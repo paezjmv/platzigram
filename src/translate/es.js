48be8c877136b0c41b494a19e91abcd8f530537e
paezjm86 <paezjm86@gmail.com>
2016-12-07 13:29:20 -0600

    Agregando un boton para subir una nueva foto

--- src/translate/es.js
+++ src/translate/es.js
@@ -14,6 +14,8 @@ module.exports = {
    'signup.have-account': '¿Tienes una cuenta?',
    'signin': ' Entrar',
    'signin.not-have-account': '¿No tienes una cuenta?',
-   'language': 'Idioma'
+   'language': 'Idioma',
+   'upload-picture': ' Subir foto',
+   'upload': ' Subir'
 }
 
