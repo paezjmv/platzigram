1914a43511378017ff11ce3b566730c51b9b4daa
paezjm86 <paezjm86@gmail.com>
2016-12-27 22:03:00 -0600

    Se agrego perfil del usuario

--- server.js
+++ server.js
@@ -25,6 +25,10 @@ app.get('/', function (req, res){
    res.render('index', { title: 'Platzigram'});
 })
 
+app.get('/:username', function (req, res, next) {
+
+})
+
 app.get('/signup', function (req, res){
    res.render('index', {title: 'Platzigram - Signup'});
 })
@@ -57,10 +61,8 @@ app.get('/api/pictures', function (req, res) {
       }
    ];
 
-   setTimeout(function(){
-      res.send(pictures);
-   }, 2000)
-})
+   res.send(pictures);
+});
 
 app.post('/api/pictures', function (req, res) {
    upload(req, res, function (err) {
@@ -69,6 +71,51 @@ app.post('/api/pictures', function (req, res) {
       }
       res.send('File uploaded');
    })
+})
+
+app.get('/api/user/:username', function (req, res) {
+  const user = {
+    username: 'paezjm86',
+    avatar: 'https://pbs.twimg.com/profile_images/583707183180685313/z-K-gc3h.jpg',
+    pictures: [
+      {
+        id: 1,
+        src: 'https://carlosazaustre.es/blog/content/images/2015/02/B9wLPUEIUAAnC-1.jpg',
+        likes: 3
+      },
+      {
+        id: 2,
+        src: 'http://www.centrodeinnovacionbbva.com/sites/default/files/freddy_and_christian.jpg',
+        likes: 10
+      },
+      {
+        id: 3,
+        src: 'https://filisantillan.com/content/images/2015/09/2015-05-02-07-39-02-compressor.jpg',
+        likes: 23
+      },
+      {
+        id: 4,
+        src: 'http://mountain.partners/wp-content/uploads/news/2016-01-11-platzi-founder-590px.jpg',
+        likes: 0
+      },
+      {
+        id: 5,
+        src: 'https://pbs.twimg.com/media/CYd31XTWEAAiV4O.png',
+        likes: 1
+      },
+      {
+        id: 6,
+        src: 'http://pulsosocial.com/wp-content/uploads/2016/01/freddier-cvander-studio-e1452623660723.jpg',
+        likes: 99
+      }
+    ]
+  }
+
+  res.send(user);
+})
+
+app.get('/:username', function (req, res) {
+  res.render('index', { title: `Platzigram - ${req.params.username}` })
 })
 
 app.listen(3000, function (err) {
